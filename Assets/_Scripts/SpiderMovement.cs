480f77084251d0afcaa6720449dcee9352693136
Nam Tran Trong <nam.tran-trong@ubisoft.com>
2023-10-23 08:42:29 +0700

    updated

--- Assets/_Scripts/SpiderMovement.cs
+++ Assets/_Scripts/SpiderMovement.cs
@@ -1,35 +1,50 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
-
 public class SpiderMovement : MonoBehaviour
 {
     public Transform legTarget;
-    public Transform legBody;
+    public Transform legCurrent;
     public LayerMask layerMask;
     private Vector3 _currentPosition;
+    public float _distance = 0.1f;
     RaycastHit _hit;
     void Start()
     {
         _currentPosition = legTarget.position;
     }
+    
     void Update()
     {
         legTarget.position = _currentPosition;
-        Ray ray = new Ray(legBody.position, Vector3.down); 
-
-        if (Physics.Raycast(ray, out _hit, Mathf.Infinity, layerMask))
+        if (Vector3.Distance(legTarget.position, legCurrent.position) > _distance)
         {
-            Debug.DrawRay(legBody.position, Vector3.down * _hit.distance, Color.red);
-            Debug.DrawLine(legTarget.position,_hit.point,Color.yellow);
-        } 
-    }
+            Vector3 A = legTarget.position;
+            Vector3 B = legCurrent.position;
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(_hit.point, 0.01f);
+            // Tính tâm O của đường tròn
+            Vector3 O = (A + B) / 2;
+
+            // Tính bán kính R của đường tròn
+            float R = Vector3.Distance(A, O);
+
+            // Tính góc hiện tại dựa trên thời gian
+            float angle = Time.deltaTime; // Thay đổi giá trị này để làm cho chuyển động nhanh hơn hoặc chậm hơn
+
+            // Tính tọa độ mới dựa trên góc và bán kính R
+            Vector3 offset = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * R;
+            legCurrent.position = O + offset;
+        }
     }
+    
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        var position = legCurrent.position;
+        
+        Gizmos.DrawSphere(position,0.01f);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(position, _distance);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(legTarget.position,position);
+    }
+    
 }
